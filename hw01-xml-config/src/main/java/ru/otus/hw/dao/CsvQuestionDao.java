39ea9f5ff88b9011015585cb56bc9024cbf138ff
Stanislav <Stanislav.Petrov@russianpost.ru>
2026-05-28 07:25:01 +0400

    переделан в CsvToBean

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
@@ -7,7 +8,9 @@ import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,27 +18,27 @@ import java.util.stream.Collectors;
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
+
     @Override
     public List<Question> findAll() {
-        try {
-            String fileName = fileNameProvider.getTestFileName();
-            var inputStream = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream(fileName);
+        String fileName = fileNameProvider.getTestFileName();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
+             InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
             if (inputStream == null) {
                 throw new QuestionReadException("File not found: " + fileName);
             }
-            List<QuestionDto> questionDtos = new CsvToBeanBuilder<QuestionDto>(
-                    new InputStreamReader(inputStream))
+            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
                     .withType(QuestionDto.class)
                     .withSeparator(';')
-                    .build()
-                    .parse();
+                    .build();
+
+            List<QuestionDto> questionDtos = csvToBean.parse();
             return questionDtos.stream()
                     .map(QuestionDto::toDomainObject)
                     .collect(Collectors.toList());
         } catch (Exception e) {
             throw new QuestionReadException("Error reading questions", e);
         }
+
     }
 }
