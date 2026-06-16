a13244b92821ce89400a0f5f14810e0fb7bd92b8
Stanislav <Stanislav.Petrov@russianpost.ru>
2026-06-16 12:11:56 +0400

    дз3 доработка

--- hw03-spring-boot/src/main/java/ru/otus/hw/Application.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/Application.java
@@ -3,9 +3,7 @@ package ru.otus.hw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootApplication
