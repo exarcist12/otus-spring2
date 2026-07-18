98b086fc5e5ac7c17609073d3da5b4d634763881
Stanislav <Stanislav.Petrov@russianpost.ru>
2026-07-18 21:42:28 +0400

    дз7

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
@@ -15,7 +15,15 @@ import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
