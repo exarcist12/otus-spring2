392ee81e8d047ca46b9cfd667120fb92afe9971d
Stanislav <Stanislav.Petrov@russianpost.ru>
2026-07-19 13:23:20 +0400

    дз7

--- hw07-spring-data-jpa-2/src/main/java/ru/otus/hw/models/Book.java
+++ hw07-spring-data-jpa-2/src/main/java/ru/otus/hw/models/Book.java
@@ -1,6 +1,16 @@
 package ru.otus.hw.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
