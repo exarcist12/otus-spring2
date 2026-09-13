dee04de00c338835817bc90450429e178d641f34
Stanislav <Stanislav.Petrov@russianpost.ru>
2026-09-13 19:39:30 +0400

    дз16 hateoas ссылки

--- hw16-spring-boot-actuator/src/main/java/ru/otus/hw/repositories/JdbcCommentRepository.java
+++ hw16-spring-boot-actuator/src/main/java/ru/otus/hw/repositories/JdbcCommentRepository.java
@@ -3,10 +3,12 @@ package ru.otus.hw.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "comments", path = "comments")
 public interface JdbcCommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("SELECT c FROM Comment c WHERE c.book.id = :bookId")
